4e3dd4c9ec3cf6008e9b24426418bf5718f1b094
vodkar <boombarah@gmail.com>
2023-04-05 20:36:06 +0900

    Updated tests

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+from pytest import fixture
+
+from app.app import init_app
+from app.startups.database import init_db
+
+
+@fixture
+async def app():
+    app = init_app()
+    return app
+
+@fixture
+async def http_client(aiohttp_client, app):
+    return await aiohttp_client(app)
+
+@fixture(autouse=True)
+async def prepare_db(app):
+    await init_db(app)
+    await app['db'].gino.create_all()
+
